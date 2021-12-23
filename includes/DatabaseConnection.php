359b36d63acc07cee3936f211743f029277a1dea
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2021-12-23 12:10:57 +0000

    Update DatabaseConnection.php

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
+$pdo = new PDO('mysql:host=localhost;dbname=joke_database;charset=utf8', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
