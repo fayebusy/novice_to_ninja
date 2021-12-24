50036e66ff70bdf55a305254e560e8de3db4229e
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2021-12-24 21:47:29 +0000

    Classes/OOP-EntryPoint

--- public/deletejoke.php
+++ public/deletejoke.php
@@ -3,9 +3,7 @@ try {
 	include __DIR__ . '/../includes/DatabaseConnection.php';
 	include __DIR__ . '/../classes/DatabaseTable.php';
 	$jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-	$jokesTable->delete($_POST['id']);
-
-	header('location: jokes.php');
+	
 }
 catch (PDOException $e) {
 	$title = 'An error has occurred';
