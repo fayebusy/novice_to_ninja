72894ffb82ffc84a304abfc8934fd76e543d6a64
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2021-12-28 13:59:38 +0000

    CMS/EntryPoint-Framework

--- public/index.php
+++ public/index.php
@@ -2,10 +2,12 @@
 
 try {
     include __DIR__ . '/../classes/EntryPoint.php';
+    include __DIR__ . '/../classes/JokeDBRoutes.php';
+
     
     //if no route variable is set, use 'joke/home' 
     $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
-    $entryPoint = new EntryPoint($route);
+    $entryPoint = new EntryPoint($route,new JokeDBRoutes());
     $entryPoint->run();
 } catch (PDOException $e) {
     $title = 'An error has occurred';
