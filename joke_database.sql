1e15e49b797995e99e0101f097ea6fcb67eaa71b
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2021-12-29 19:50:05 +0000

    Admin/Foreign-key

--- joke_database.sql
+++ joke_database.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: joke_database
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.21-MariaDB
+-- Server version	5.5.5-10.4.21-MariaDB-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -37,8 +37,31 @@ CREATE TABLE `author` (
 
 LOCK TABLES `author` WRITE;
 /*!40000 ALTER TABLE `author` DISABLE KEYS */;
-INSERT INTO `author` VALUES (1,'Youssoupha FAYE','fayeyousso@gmail.com','$2y$10$ewqgrTB1Yt0m1NXBVGdpM.ZAR2F1hlNKnGI5ZMCGT4gAbt8wwzCRa'),(2,'Tom Butler','tom@r.je',NULL),(5,'mohamed thiam','mothiam@gmail.com','$2y$10$2r6TJx0Ts8k.6ObWe/CJF.gsdS3D4/KXZaIyxsI2LxUvpxl.nypgy');
+INSERT INTO `author` VALUES (1,'Youssoupha FAYE','fayeyousso@gmail.com','$2y$10$ewqgrTB1Yt0m1NXBVGdpM.ZAR2F1hlNKnGI5ZMCGT4gAbt8wwzCRa'),(2,'Tom Butler','tom@r.je',NULL),(5,'mohamed thiam','mouhathiam@gmail.com','$2y$10$2r6TJx0Ts8k.6ObWe/CJF.gsdS3D4/KXZaIyxsI2LxUvpxl.nypgy');
 /*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -53,7 +76,9 @@ CREATE TABLE `joke` (
   `joketext` text DEFAULT NULL,
   `jokedate` date NOT NULL,
   `authorId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `authorId` (`authorId`),
+  CONSTRAINT `joke_ibfk_1` FOREIGN KEY (`authorId`) REFERENCES `author` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -66,6 +91,29 @@ LOCK TABLES `joke` WRITE;
 INSERT INTO `joke` VALUES (1,'How many programmers does it take to screw in a lightbulb? None, it\'s a hardware problem.','2017-04-01',1),(2,'Why did the programmer quit his job? He didn\'t get arrays','2017-04-01',1),(3,'Why was the empty array stuck outside? It didn\'t have any keys','2017-04-01',2),(4,'this is a new joke','2021-12-24',1),(6,'pour faire peur a un informaticien dites quil a oublie un point virgule ','2021-12-28',1),(7,'thiam ne sait pas faire de blague','2021-12-29',5);
 /*!40000 ALTER TABLE `joke` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `joke_category`
+--
+
+DROP TABLE IF EXISTS `joke_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `joke_category` (
+  `jokeId` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  PRIMARY KEY (`jokeId`,`categoryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `joke_category`
+--
+
+LOCK TABLES `joke_category` WRITE;
+/*!40000 ALTER TABLE `joke_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `joke_category` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -76,4 +124,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-29 18:13:24
+-- Dump completed on 2021-12-29 19:48:50
