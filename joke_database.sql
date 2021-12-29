5721418c3ea68f1f1baa0d154079393a7263e7cb
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2021-12-29 18:12:58 +0000

    test

--- joke_database.sql
+++ joke_database.sql
@@ -1,104 +1,79 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.27, for macos10.15 (x86_64)
 --
--- Hôte : localhost
--- Généré le : mar. 28 déc. 2021 à 18:40
--- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 8.1.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: joke_database
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.21-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données : `joke_database`
+-- Table structure for table `author`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `author`
---
-
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `author` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `author`
+-- Dumping data for table `author`
 --
 
-INSERT INTO `author` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Youssoupha FAYE', 'fayeyousso@gmail.com', '$2y$10$ewqgrTB1Yt0m1NXBVGdpM.ZAR2F1hlNKnGI5ZMCGT4gAbt8wwzCRa'),
-(2, 'Tom Butler', 'tom@r.je', NULL);
-
--- --------------------------------------------------------
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Youssoupha FAYE','fayeyousso@gmail.com','$2y$10$ewqgrTB1Yt0m1NXBVGdpM.ZAR2F1hlNKnGI5ZMCGT4gAbt8wwzCRa'),(2,'Tom Butler','tom@r.je',NULL),(5,'mohamed thiam','mothiam@gmail.com','$2y$10$2r6TJx0Ts8k.6ObWe/CJF.gsdS3D4/KXZaIyxsI2LxUvpxl.nypgy');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `joke`
+-- Table structure for table `joke`
 --
 
+DROP TABLE IF EXISTS `joke`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `joke` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `joketext` text DEFAULT NULL,
   `jokedate` date NOT NULL,
-  `authorId` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `authorId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `joke`
+-- Dumping data for table `joke`
 --
 
-INSERT INTO `joke` (`id`, `joketext`, `jokedate`, `authorId`) VALUES
-(1, 'How many programmers does it take to screw in a lightbulb? None, it\'s a hardware problem.', '2017-04-01', 1),
-(2, 'Why did the programmer quit his job? He didn\'t get arrays', '2017-04-01', 1),
-(3, 'Why was the empty array stuck outside? It didn\'t have any keys', '2017-04-01', 2),
-(4, 'this is a new joke', '2021-12-24', 1),
-(6, 'pour faire peur a un informaticien dites quil a oublie un point virgule ', '2021-12-28', 1);
+LOCK TABLES `joke` WRITE;
+/*!40000 ALTER TABLE `joke` DISABLE KEYS */;
+INSERT INTO `joke` VALUES (1,'How many programmers does it take to screw in a lightbulb? None, it\'s a hardware problem.','2017-04-01',1),(2,'Why did the programmer quit his job? He didn\'t get arrays','2017-04-01',1),(3,'Why was the empty array stuck outside? It didn\'t have any keys','2017-04-01',2),(4,'this is a new joke','2021-12-24',1),(6,'pour faire peur a un informaticien dites quil a oublie un point virgule ','2021-12-28',1),(7,'thiam ne sait pas faire de blague','2021-12-29',5);
+/*!40000 ALTER TABLE `joke` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `joke`
---
-ALTER TABLE `joke`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `author`
---
-ALTER TABLE `author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT pour la table `joke`
---
-ALTER TABLE `joke`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-29 17:31:46
